50464c4a371dc303481612ecc094aeaec94bdd92
Bryan Mark Fajutag <bryan.fajutag@adish.co.jp>
2017-03-02 14:59:58 +0800

    Update roman.rb
    Optimized the code for better understand

--- roman.rb
+++ roman.rb
@@ -11,14 +11,11 @@ roman_converter = [["I", 1], ["IV", 4], ["V", 5], ["IX", 9], ["X", 10], ["XL", 4
                   ["CM", 900], ["M", 1_000]].reverse
 
 roman_number = []
-while number != 0                                   #loop until number is 0
-  roman_converter.each_with_index do |key, index|   #key is the array that contains the roman numeral and it's value in integer, index is the offset of the array
-    if number >= roman_converter[index][1].to_i     #check if number is greater than or equal
-      number -= roman_converter[index][1].to_i      #subtract the value of roman numeral
-      roman_number.push(roman_converter[index][0])  #push the roman numeral to the array
-      break                                         #break to reset the loop of the combinations in roman numeral
-    end
+roman_converter.each do |value|     #value is the array that contains the roman numeral and it's value in integer
+  while number-value[1] >= 0        #loop check if number is greater than or equal
+    number = number - value[1]      #subtract the value of roman numeral
+    roman_number.push(value[0])     #push the roman numeral to the array
   end
 end
 
-puts roman_number.join('')                          #print the array, joining each value
+puts roman_number.join('')          #print the array, joining each value
